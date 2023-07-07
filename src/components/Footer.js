396266899089669098fd40cebad7b2d7c4d1b796
Maera <baeitsmae@gmail.com>
2023-07-07 12:10:26 +0200

    details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer() {
     return (
         <footer>
             <p><i className="fas fa-phone"></i> 07.58.57.42.12</p>
-            <p>©️Copyrights 2021 - LP Studio. Tous droits réservés.</p>
-            <p><i className="fas fa-envelope"></i><Link to="mailto:contact@lpstudio.com" target="_blank"> contact@lpstudio.com </Link></p>
+            <p>©️Copyrights 2021 - LP Photographie. Tous droits réservés.</p>
+            <p><i className="fas fa-envelope"></i><Link to="mailto:contact@lp-photographie.com" target="_blank"> contact@lp-photograpie.com </Link></p>
         </footer>
     )
 }
